0075f8d946c95209339da8b0890ce519d39bcf9a
David Lechner <d_lechner_94@gmx.at>
2019-10-21 18:01:24 +0200

    refactored pre processing chain

--- work_the_magic.sh
+++ work_the_magic.sh
@@ -16,12 +16,11 @@ echo "created directory ${dir_name}"
 echo ""
 
 python3 data_transformation/01_to_hdf5.py -m data/mf4 -o "${dir_name}/stage1" -d $time 1> "${dir_name}/stage1/out.std" 2> "${dir_name}/stage1/out.err"
-python3 data_transformation/02_transform_signals.py -i "${dir_name}/stage1" -o "${dir_name}/stage2" 1>"${dir_name}/stage2/out.std" 2> "${dir_name}/stage2/out.err"
-python3 data_transformation/03_window_size.py -i "${dir_name}/stage2" -o "${dir_name}/stage3" -s $window_size 1> "${dir_name}/stage3/out.std" 2> "${dir_name}/stage3/out.err"
-python3 data_transformation/04_remove_0_speed.py -i "${dir_name}/stage3" -o "${dir_name}/stage4" 1> "${dir_name}/stage4/out.std" 2> "${dir_name}/stage4/out.err"
-python3 data_transformation/05_add_class_column.py -i "${dir_name}/stage4" -o "${dir_name}/stage5" 1> "${dir_name}/stage5/out.std" 2> "${dir_name}/stage5/out.err"
+python3 data_transformation/02_window_size.py -i "${dir_name}/stage1" -o "${dir_name}/stage2" -s $window_size 1> "${dir_name}/stage2/out.std" 2> "${dir_name}/stage2/out.err"
+python3 data_transformation/03_remove_0_speed.py -i "${dir_name}/stage2" -o "${dir_name}/stage3" 1> "${dir_name}/stage3/out.std" 2> "${dir_name}/stage3/out.err"
+python3 data_transformation/04_add_class_column.py -i "${dir_name}/stage3" -o "${dir_name}/stage4" 1> "${dir_name}/stage4/out.std" 2> "${dir_name}/stage4/out.err"
 echo "Let the magician do his work."
-python magic/magic.py -i "${dir_name}/stage5"
+python magic/magic.py -i "${dir_name}/stage4"
 
 if [[ "$delete" -eq "1" ]];
 then
